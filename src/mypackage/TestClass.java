32c06d95ec892cc2060c0e1f6bbbd5eaf0867b51
RodionovaL <rodionovalyda64@gmail.com>
2021-12-24 15:05:57 +0200

    1 commit

---
+++ src/mypackage/TestClass.java
@@ -0,0 +1,32 @@
+package mypackage;
+
+import java.sql.SQLOutput;
+
+public class TestClass {
+
+    public static void main(String[] args) {
+
+                      //HW Week 1: Логические и условные операторы (if, if-else, switch )
+        int a = 10;
+        int b = 15;
+        int c = 20;
+        int d = 25;
+
+        System.out.println();
+        System.out.println("Даны 4 числа типа int. Сравнить их и вывести наименьшее на консоль");
+
+        if (a < b && a < c && a < d) {
+            System.out.println("Min value - " + a);
+        } else if (b > a && b > c && b > d) {
+            System.out.println("Min value - " + b);
+        } else if (c > a && c > b && c > d) {
+            System.out.println("Min value - " + c);
+        } else if (d > a && d > b && d > c) {
+            System.out.println("Min value - " + d);
+        }
+
+        int answer = Math.min( Math.min(a,b), Math.min(c,d) );
+        System.out.println(answer);
+    }
+
+}
