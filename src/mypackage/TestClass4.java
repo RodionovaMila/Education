73b33a7bad73a8ca4fb517603fb8058642540c15
RodionovaL <rodionovalyda64@gmail.com>
2021-12-30 13:15:04 +0200

    HW Week 1 п.4

---
+++ src/mypackage/TestClass4.java
@@ -0,0 +1,18 @@
+package mypackage;
+
+import java.sql.SQLOutput;
+
+public class TestClass4 {
+
+
+    public static void main(String[] args) {
+        String name1 = "Людмила";
+        String name2 = "Людмила";
+        if (name1 == name2) {
+            System.out.println("Тезки");
+        } else
+            System.out.println("Разные имена");
+
+    }
+}
+
