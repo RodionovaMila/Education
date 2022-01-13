d347d34f2ff7e98272327cc8820fda2e186e2843
RodionovaMila <96572404+RodionovaMila@users.noreply.github.com>
2022-01-13 12:14:23 +0200

    Add files via upload

--- src/mypackage/TestClass41.java
+++ src/mypackage/TestClass41.java
@@ -1,27 +1,25 @@
 package mypackage;
 
-import java.sql.SQLOutput;
-
-public class TestClass4 {
+public class TestClass41 {
 
 
     public static void main(String[] args) {
 
         //HW Week 1 п.3 : Логические и условные операторы (if, if-else, switch )
 
-        String name1 = "людмила";
-        String name2 = "Людмила";
+        String name1 = "  Людмила";
+        String name2 = "Людмила   ";
 
         System.out.println();
         System.out.println("Даны имена 2х человек (тип String). Если имена равны, то вывести сообщение о том, " +
                 "что люди являются тезками.");
 
-        if (name1.equalsIgnoreCase(name2)) {
-            System.out.println("Люди являются тезками: " + name1.toUpperCase());
-        } else {
-            System.out.println("Имя первого : " + name1 + "\nameИмя второго : " + name2);
-        }
+        System.out.println(name1);
+        System.out.println(name2.trim());
 
     }
 
 }
+
+
+
