3553e597ab357b8f7be5e9e14ee6e4e653fdf16f
RodionovaL <rodionovalyda64@gmail.com>
2022-02-07 15:17:28 +0200

    HW Week 1 п.4 : МАССИВЫ

--- src/mypackage/Arrays5.java
+++ src/mypackage/Arrays5.java
@@ -1,24 +1,23 @@
 package mypackage;
 import java.util.Arrays;
 
-
 public class Arrays5 {
-
-
     public static void main(String[] args) {
-        int[] arr = {15, 26, 144, 716, 999, 155, 326, 845, 632, 956, 125, 426, 746, 255, 9, 1, 150, 328, 856, 789};
+        int [] arr = new int[10];
+
 
         boolean isSorted = false;
         int buf;
-        while (!isSorted) {
+        while(!isSorted) {
             isSorted = true;
-            for (int i = 0; i < arr.length - 1; i++) {
-                if (arr[i] > arr[i + 1]) {
+            for (int i = 0; i < arr.length-1; i++) {
+                arr[i] =  (int)(Math.random() * 100);
+                if(arr[i] < arr[i+1]){
                     isSorted = false;
 
                     buf = arr[i];
-                    arr[i] = arr[i + 1];
-                    arr[i + 1] = buf;
+                    arr[i] = arr[i+1];
+                    arr[i+1] = buf;
                 }
             }
         }
